2ec9e02e1927cfe5abd84a5afd0a43b9a53a4cff
Jason Fournier <jmf4nier@hotmail.com>
2019-04-10 18:11:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board[index] == " "
-  return false
+  if board[index] == " "
+    return false
+  else true 
+  end
 end

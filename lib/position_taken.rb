c6e0b1b07094068cf07c9b2b713a1a7dfd8ad7f5
Jason Fournier <jmf4nier@hotmail.com>
2019-04-10 18:27:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,7 +6,7 @@ board = [" ", "X", " ", " "]
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
-  elsif board[index] == "X" || "O"
+  elsif board[index] == "X" #|| "O"
     return true 
   end
 end
